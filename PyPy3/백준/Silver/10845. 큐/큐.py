0ea49fa283823d01a7451ed38fcdccde79f29789
urwizard-9 <134476772+urwizard-9@users.noreply.github.com>
2026-01-05 20:03:30 +0900

    [Silver IV] Title: 큐, Time: 420 ms, Memory: 113356 KB -BaekjoonHub

---
+++ PyPy3/백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+s = deque()
+n=int(input())
+for x in range(n):
+    command = list(input().split())
+    order = command[0]
+    if(command[0]=="push"):
+        num = int(command[1])
+        s.append(num)
+    elif(command[0]=="front"):
+        print(s[0] if len(s)>0 else -1 )
+    elif(command[0]=="back"):
+        print(s[-1] if len(s)>0 else -1 )
+    elif(command[0]=="size"):
+        print(len(s))
+    elif(command[0]=="empty"):
+        print(1 if len(s)<=0 else 0)
+    elif(command[0]=="pop"):
+        print(s.popleft() if len(s)>0 else -1)
